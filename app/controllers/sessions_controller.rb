02b77f5d9d8237ddd0b0809b30170bf0fc47e658
vagrant <mikesnow444@gmail.com>
2019-02-21 20:53:23 +0000

    added session routes and controller to tracck if user is logged in or not

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+
+    #check if email exists in db and if password is correct
+    if user && user.authenticate(params[:password])
+      #save user id in a cookie
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      #if login is not valid
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+
+end
