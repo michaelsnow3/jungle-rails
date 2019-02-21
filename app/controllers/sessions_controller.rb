827d266b7e64ce3c32912fac7e73fbc60744d905
vagrant <mikesnow444@gmail.com>
2019-02-21 22:21:24 +0000

    added login, logout, register links to nav bar. shows register and login if user is logged out and logout otherwise

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:email])
 
     #check if email exists in db and if password is correct
-    if user && user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       #save user id in a cookie
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       #if login is not valid
