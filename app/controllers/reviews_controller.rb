1980d8ad5739f7588fcc49ce6a9a06e394610cc3
vagrant <mikesnow444@gmail.com>
2019-02-23 22:18:03 +0000

    added review controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+
+  def create
+    description = params["description"]
+    rating = params["review"]["rating"]
+    product_id = params["product_id"]
+    user_id = session[:user_id]
+
+    @review = Review.new(
+      description: description, 
+      rating: rating, 
+      product_id: product_id,
+      user_id: user_id
+    )
+    @review.save
+    redirect_to :back
+  end
+
+end
